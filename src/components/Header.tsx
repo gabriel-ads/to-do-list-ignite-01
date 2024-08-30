2feeb948c0d92925a521a7fc501bb84e85874f84
Gabriel Alves <gabriel.alves450@hotmail.com>
2024-08-30 16:37:06 -0300

    Created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+import toDoLogo from '../assets/to-do-logo.svg'
+
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={toDoLogo} alt='Logotipo do aplicativo to do List'/>
+    </header>
+  )
+}
